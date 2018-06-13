46ff037eeff021537ae4d5ea133b8354c9085136
Weihai Li <windseeker2011@163.com>
2018-06-13 18:26:48 +0800

    The default SpanStore is in-memory (good for demos and getting started quickly). For a more robust solution you can add MySQL and spring-boot-starter-jdbc to your classpath and enable the JDBC SpanStore via configuration, e.g.:

---
+++ spring-cloud-sleuth-http-mysql-server/src/main/java/com/windseeker2011/cloud/sleuth/zipkin/ZipkinServerApplication.java
@@ -0,0 +1,25 @@
+package com.windseeker2011.cloud.sleuth.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+import zipkin.server.EnableZipkinServer;
+
+/**
+ * Zipkin Server
+ * 
+ * @author Weihai Li
+ *
+ */
+@EnableEurekaClient
+@EnableZipkinServer
+@SpringBootApplication
+public class ZipkinServerApplication {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(ZipkinServerApplication.class, args);
+
+	}
+
+}
